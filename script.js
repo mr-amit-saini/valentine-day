17f8fb5a686fdd425e5d1e5985288892710978fc
Amit Saini <73197370+mr-amit-saini@users.noreply.github.com>
2026-02-12 13:39:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const gifStages = [
-  "https://tenor.com/view/love-gif-17552840805493029515",     // 0 normal
-  "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif", // 1 confused
-  "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",           // 2 pleading
+  "https://media1.tenor.com/m/85g3trJkuosAAAAC/love.gif",     // 0 normal
+  "https://media.tenor.com/cd-Tnw2IxD0AAAAi/hachiware-dance.gif",   //1 dance
+   "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",           // 2 pleading
+  "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif", // 2.5 confused 
   "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",           // 3 sad
   "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",      // 4 sadder
   "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",           // 5 devastated
